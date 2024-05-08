1261ebbbd1f8b2f32855f72b94940d5882c45f1e
potawk <j--po.km-82kto@nifty.com>
2024-05-08 05:59:32 +0000

    非ログインTopPageとログイン後TopPageを作成

---
+++ resources/views/task/list.blade.php
@@ -0,0 +1,70 @@
+@extends('layout')
+
+{{-- タイトル --}}
+@section('title')(詳細画面)@endsection
+
+{{-- メインコンテンツ --}}
+@section('contes')
+		<h1>タスクの登録(未実装)</h1>
+			<form action="./top.html" method="post">
+				タスク名：<input><br>
+				期限：<input type="date"><br>
+				タスク詳細：<textarea></textarea><br>
+				重要度：<label><input type="radio" name="priority">低い</label> /
+								<label><input type="radio" name="priority" checked>普通</label> /
+								<label><input type="radio" name="priority">高い</label><br>
+				<button>タスクを登録する</button>
+			</form>
+			
+		<h1>タスクの一覧(未実装)</h1>
+		<a href="./top.html">CSVダウンロード(未実装)</a><br>
+		<table border="1">
+			<tr>
+				<th>タスク名</th>
+				<th>期限</th>
+				<th>重要度</th>
+			</tr>
+			<tr>
+				<td>HTML formの学習</td>
+				<td>2022/01/01</td>
+				<td>普通</td>
+				<td><a href="./detail.html">詳細閲覧</a></td>
+				<td><a href="./edit.html">編集</a></td>
+				<td><form action="./top.html"><button>完了</button></form></td>
+			</tr>
+			<tr>
+				<td>PHPの学習</td>
+				<td>2022/01/15</td>
+				<td>普通</td>
+				<td><a href="./detail.html">詳細閲覧</a></td>
+				<td><a href="./edit.html">編集</a></td>
+				<td><form action="./top.html"><button>完了</button></form></td>
+			</tr>
+			<tr>
+				<td>RDBの学習</td>
+				<td>2022/02/01</td>
+				<td>普通</td>
+				<td><a href="./detail.html">詳細閲覧</a></td>
+				<td><a href="./edit.html">編集</a></td>
+				<td><form action="./top.html"><button>完了</button></form></td>
+			</tr>
+			<tr>
+				<td>Laravelの学習</td>
+				<td>2022/02/15</td>
+				<td>普通</td>
+				<td><a href="./detail.html">詳細閲覧</a></td>
+				<td><a href="./edit.html">編集</a></td>
+				<td><form action="./top.html"><button>完了</button></form></td>
+			</tr>
+		</table>
+		<!-- ページネーション -->
+		現在 1 ページ目<br>
+		<a href="./top.html">最初のページ(未実装)</a> /
+		<a href="./top.html">前に戻る(未実装)</a> / 
+		<a href="./top.html">次に進む(未実装)</a>
+		<br>
+		<hr>
+		<menu label="リンク">
+			<a href="./index.html">ログアウト(未実装)</a>
+		</menu>
+@endsection
