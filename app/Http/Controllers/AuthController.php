fbd42ef17414d2d7736f3f710d89a3a6ee86d6c1
potawk <j--po.km-82kto@nifty.com>
2024-05-10 08:34:17 +0000

    認証の「formデータを受け取る」所まで書いた

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,6 +3,7 @@ declare(strict_types=1);
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\LoginPostRequest;
 
 class AuthController extends Controller
 {
@@ -15,4 +16,19 @@ class AuthController extends Controller
     {
         return view('index');
     }
+    
+    /**
+     * ログイン処理
+     * 
+     */
+     public function login(LoginPostRequest $request)
+     {
+         // validate済
+         
+         // データの取得
+         $datum = $request->validated();
+         
+         //
+         var_dump($datum); exit;
+     }
 }
