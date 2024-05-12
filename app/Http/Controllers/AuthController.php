70db98500a089e69fa9f1664baea58a77f1c54df
potawk <j--po.km-82kto@nifty.com>
2024-05-12 19:49:49 +0000

    「task/list」画面に遷移しません

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,31 +4,42 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\LoginPostRequest;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
     /**
      * トップページ を表示する
-     *
+     * 
      * @return \Illuminate\View\View
      */
     public function index()
     {
         return view('index');
     }
-    
+
     /**
      * ログイン処理
      * 
      */
-     public function login(LoginPostRequest $request)
-     {
-         // validate済
-         
-         // データの取得
-         $datum = $request->validated();
-         
-         //
-         var_dump($datum); exit;
-     }
+    public function login(LoginPostRequest $request)
+    {
+        // validate済
+
+        // データの取得
+        $datum = $request->validated();
+        //var_dump($datum); exit;
+
+        // 認証
+        if (Auth::attempt($datum) === false) {
+            return back()
+                   ->withInput() // 入力値の保持
+                   ->withErrors(['auth' => 'emailかパスワードに誤りがあります。',]) // エラーメッセージの出力
+                   ;
+        }
+
+        //
+        $request->session()->regenerate();
+        return redirect()->intended('/task/list');
+    }
 }
