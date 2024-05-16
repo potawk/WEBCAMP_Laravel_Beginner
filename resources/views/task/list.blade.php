f9910f2f54f9c014c88bca8dc0e6ad1d43a8743d
potawk <j--po.km-82kto@nifty.com>
2024-05-16 02:03:20 +0000

    タスクの削除と完了を書いた

--- resources/views/task/list.blade.php
+++ resources/views/task/list.blade.php
@@ -5,10 +5,19 @@
 
 {{-- メインコンテンツ --}}
 @section('contets')
-<h1>タスクの登録</h1>
-	@if (session('front.task_register_success') == true)
-		タスクを登録しました！！<br>
-	@endif
+        <h1>タスクの登録</h1>
+            @if (session('front.task_register_success') == true)
+                タスクを登録しました！！<br>
+            @endif
+            @if (session('front.task_delete_success') == true)
+                タスクを削除しました！！<br>
+            @endif
+            @if (session('front.task_completed_success') == true)
+                タスクを完了にしました！！<br>
+            @endif
+            @if (session('front.task_completed_failure') == true)
+                タスクの完了に失敗しました....<br>
+            @endif
             @if ($errors->any())
                 <div>
                 @foreach ($errors->all() as $error)
@@ -26,49 +35,47 @@
                     <label><input type="radio" name="priority" value="3" @if (old('priority') == 3) checked @endif>高い</label><br>
                 <button>タスクを登録する</button>
             </form>
-            
-		<h1>タスクの一覧(未実装)</h1>
-		<a href="./top.html">CSVダウンロード(未実装)</a><br>
-		<table border="1">
-			<tr>
-				<th>タスク名</th>
-				<th>期限</th>
-				<th>重要度</th>
-			</tr>
+
+        <h1>タスクの一覧</h1>
+        <a href="./top.html">CSVダウンロード(未実装)</a><br>
+        <table border="1">
+        <tr>
+            <th>タスク名
+            <th>期限
+            <th>重要度
 @foreach ($list as $task)
-			<tr>
-				<td>{{ $task->name }}</td>
-				<td>{{ $task->period }}</td>
-				<td>{{ $task->getPriorityString() }}</td>
-				<td><a href="{{ route('detail', ['task_id' => $task->id]) }}">詳細閲覧</a></td>
-				<td><a href="{{ route('edit', ['task_id' => $task->id]) }}">編集</a></td>
-				<td><form action="./top.html"><button>完了</button></form></td>
-			</tr>
+        <tr>
+            <td>{{ $task->name }}
+            <td>{{ $task->period }}
+            <td>{{ $task->getPriorityString() }}
+            <td><a href="{{ route('detail', ['task_id' => $task->id]) }}">詳細閲覧</a>
+            <td><a href="{{ route('edit', ['task_id' => $task->id]) }}">編集</a>
+            <td><form action="{{ route('complete', ['task_id' => $task->id]) }}" method="post"> @csrf <button onclick='return confirm("このタスクを「完了」にします。よろしいですか？");' >完了</button></form>
 @endforeach
-		</table>
-		<!-- ページネーション -->
-		{{-- {{ $list->links() }} --}}
-		現在 {{ $list->currentPage() }} ページ目<br>
-		@if ($list->onFirstPage() === false)
-		<a href="/task/list">最初のページ</a>
-		@else
-		最初のページ
-		@endif
-		/
-		@if ($list->previousPageUrl() !== null)
-			<a href="{{ $list->previousPageUrl() }}">前に戻る</a>
-		@else
-			前に戻る
-		@endif
-		/
-		@if ($list->nextPageUrl() !== null)
-			<a href="{{ $list->nextPageUrl() }}">次に進む</a>
-		@else
-			次に進む
-		@endif
-		<br>
-		<hr>
-		<menu label="リンク">
-			<a href="/logout">ログアウト</a><br>
-		</menu>
+        </table>
+        <!-- ページネーション -->
+        {{-- {{ $list->links() }} --}}
+        現在 {{ $list->currentPage() }} ページ目<br>
+        @if ($list->onFirstPage() === false)
+            <a href="/task/list">最初のページ</a>
+        @else
+            最初のページ
+        @endif
+        /
+        @if ($list->previousPageUrl() !== null)
+            <a href="{{ $list->previousPageUrl() }}">前に戻る</a>
+        @else
+            前に戻る
+        @endif
+        /
+        @if ($list->nextPageUrl() !== null)
+            <a href="{{ $list->nextPageUrl() }}">次に進む</a>
+        @else
+            次に進む
+        @endif
+        <br>
+        <hr>
+        <menu label="リンク">
+        <a href="/logout">ログアウト</a><br>
+        </menu>
 @endsection
